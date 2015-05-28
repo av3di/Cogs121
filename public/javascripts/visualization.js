207d84dc59a4c5140d3841e0fea33f614893bced
Roberto Reyes <robreys123@gmail.com>
2015-05-27 18:53:05 -0700

    node app and basic template

---
+++ public/javascripts/visualization.js
@@ -0,0 +1,55 @@
+// Create the Google Map…
+var map = new google.maps.Map(d3.select("#map").node(), {
+  zoom: 10,
+  center: new google.maps.LatLng(32.7150, -117.1625),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+// Load the station data. When the data comes back, create an overlay.
+d3.json("stations.json", function(data) {
+  var overlay = new google.maps.OverlayView();
+
+  // Add the container when the overlay is added to the map.
+  overlay.onAdd = function() {
+    var layer = d3.select(this.getPanes().overlayLayer).append("div")
+        .attr("class", "stations");
+
+    // Draw each marker as a separate SVG element.
+    // We could use a single SVG, but what size would it have?
+    overlay.draw = function() {
+      var projection = this.getProjection(),
+          padding = 20;
+
+      var marker = layer.selectAll("svg")
+          .data(d3.entries(data))
+          .each(transform) // update existing markers
+        .enter().append("svg:svg")
+          .each(transform)
+          .attr("class", "marker");
+
+      // Add a circle.
+      marker.append("svg:circle")
+          .attr("r", 10)
+          .attr("cx", padding)
+          .attr("cy", padding);
+
+      // Add a label.
+      marker.append("svg:text")
+          .attr("x", padding + 7)
+          .attr("y", padding)
+          .attr("dy", ".31em")
+          .text(function(d) { return d.key; });
+
+      function transform(d) {
+        d = new google.maps.LatLng(d.value[0], d.value[1]);
+        d = projection.fromLatLngToDivPixel(d);
+        return d3.select(this)
+            .style("left", (d.x - padding) + "px")
+            .style("top", (d.y - padding) + "px");
+      }
+    };
+  };
+
+  // Bind our overlay to the map…
+  overlay.setMap(map);
+});
