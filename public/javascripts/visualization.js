23f9221ddc6d34123bc9fb4829f35a485c643bc6
ROYCE GEE <royce.xpenser@gmail.com>
2015-05-31 14:32:09 -0700

    added slider/time

--- public/javascripts/visualization.js
+++ public/javascripts/visualization.js
@@ -1,3 +1,24 @@
+//slider
+//slider
+  $(function() {
+    $( "#slider" ).slider({
+      min:0,
+      max:1440,
+      step:60,
+      slide: function(e,ui) {
+        var hours = Math.floor(ui.value/60);
+        var minutes = ui.value - (hours*60);
+
+        if(String(hours).length==1) {hours = '0' + hours;}
+        if(String(minutes).length==1) {minutes = '0' + minutes;}
+
+        $('#the-time').html('Time: '+hours+':'+minutes);
+      }}
+    )
+  });
+
+
+
 // Create the Google Map…
 var map = new google.maps.Map(d3.select("#map").node(), {
   zoom: 10,
